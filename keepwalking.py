7e9a746f3a49242ee342acb670ae173041645f77
Sasha Rosenfeld <87139946+sasharosenfeld@users.noreply.github.com>
2022-06-04 20:34:11 +0000

    Did minor edits

--- keepwalking.py
+++ keepwalking.py
@@ -1,8 +1,8 @@
 print("\nThe footsteps stop at some point, but you’re too distracted by a small dog in front of you to realize. It looks very sad.")
-choiceB1 = input("\nEnter A to lean down to check on the dog or B to mind your own business.").upper()
+choiceB1 = input("\nEnter A to lean down to check on the dog or B to mind your own business: ").upper()
 while choiceB1 != "A" and choiceB1 != "B":
   print("That is not a valid option.")
-  choiceB1 = input("\nEnter A to lean down to check on the dog or B to mind your own business.").upper()
+  choiceB1 = input("\nEnter A to lean down to check on the dog or B to mind your own business: ").upper()
 if choiceB1 == "A":
   print("\nThe dog IS sad. It’s sad because it is incredibly hungry. Fortunately for the dog, you provide quite a bit of meat. Your pitiful life has come to an end!")
 elif choiceB1 == "B":
--- walk.py
+++ walk.py
@@ -1,10 +1,10 @@
 #Use of this page is optional. If you use code here, make sure either import page2 or from page2 import * appear on your main.py page.
 
-print("You think you hear footsteps behind you, but can’t see anyone.")
-choiceB = input("Enter A for keep walking or B for go into a shop:  ").upper()
+print("\nYou think you hear footsteps behind you, but can’t see anyone.")
+choiceB = input("\nEnter A for keep walking or B for go into a shop: ").upper()
 while choiceB != "A" and choiceB != "B":
-  print("That is not a valid option.")
-  choiceB = input("Enter A for keep walking or B for go into a shop.").upper()
+  print("\nThat is not a valid option.")
+  choiceB = input("\nEnter A for keep walking or B for go into a shop: ").upper()
 if choiceB == "A":
   import keepwalking
 elif choiceB == "B":
