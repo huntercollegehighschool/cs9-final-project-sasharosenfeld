e9695b5f97b380f03ce977d2c9977db6b98ebdb7
Sasha Rosenfeld <87139946+sasharosenfeld@users.noreply.github.com>
2022-06-04 19:40:03 +0000

    Started

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ Name of Project:
 #import page2  # uncomment if you're using page2
 #import page3  # uncomment if you're using page3
 #import page4  # uncomment if you're using page4
+
+def start():
+  print("You are an average person. Not very smart or fun. You don't have many friends. Your entire life is your job at the local Starbucks, where you barely interact with your coworkers. One day, on your way home, you feel almost like someone is watching you. How strange: No one ever pays attention to you. You usually walk home, but maybe the bus would be safer today...")
+  choice = input("Enter A to take the bus or B to walk home: ")
+  if choice == "A":
+    print("You get on the bus. You still feel someone watching you. Do you: A) Pretend everything is fine or B) Ask the bus driver for help. ")
+    #choiceA == input ("Enter ")
+  elif choice == "B":
+    print("")
