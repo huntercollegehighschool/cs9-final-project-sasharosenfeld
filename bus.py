efb20d0df442ee093dc64cc89e83d87969a808ed
Sasha Rosenfeld <87139946+sasharosenfeld@users.noreply.github.com>
2022-06-10 23:45:16 +0000

    Formatting

--- bus.py
+++ bus.py
@@ -1,5 +1,3 @@
-#Use of this page is optional. If you use code here, make sure either import page1 or from page1 import * appear on your main.py page.
-
 print("\nYou wait for the bus, stll feeling the eyes on you. When it finally arrives, the doors open with a groan. The bus driver does not look at you as you get on. It's practically empty, and you sit down near the window. You still feel someone watching you. Maybe someone else can help...")
 choiceA = input("\nEnter A to pretend everything is fine or B to ask the bus driver for help: ").upper()
 while choiceA != "A" and choiceA != "B":
--- pretend.py
+++ pretend.py
@@ -1,5 +1,3 @@
-#Use of this page is optional. If you use code here, make sure either import page3 or from page3 import * appear on your main.py page.
-
 print("\nYou try to pretend everything is fine. Maybe the gray of the cty will take your mind off things? You look out the window and see the reflection of a person behind you. They look pretty pale...")
 choiceP = input("\nEnter A to mind your own business or B to check on the person: ").upper()
 while choiceP != "A" and choiceP != "B":
