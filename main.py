d57f8f9f72af45503a4a69ed18e2fe988c9c6b77
Sasha Rosenfeld <87139946+sasharosenfeld@users.noreply.github.com>
2022-06-10 18:03:12 +0000

    Added welcome

--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 Name(s): Serena Taxin, Sasha Rosenfeld
 Name of Project: Minding Your Business
 """
-print("\nYou are an average person. Not very smart or fun. You don't have many friends. Your entire life is your job at the local Starbucks, where you barely interact with your coworkers. One day, on your way home, you feel almost like someone is watching you. How strange: No one ever pays attention to you. You usually walk home, but maybe the bus would be safer today...\n")
-
-choice1 = input("Enter A to take the bus or B to walk home: ").upper()
-
-while choice1 != "A" and choice1 != "B":
-  print("\nThat is not a valid option.")
-  choice1 = input("\nEnter A to take the bus or B to walk home:  ").upper()
-if choice1 == "A":
-  import bus
-elif choice1 == "B":
-  import walk
+welcome = input("Welcome to Minding Your Business. Enter Y to play: ").upper()
+if welcome == "Y":
+  print("\nYou are an average person. Not very smart or fun. You don't have many friends. Your entire life is your job at the local Starbucks, where you barely interact with your coworkers. One day, on your way home, you feel almost like someone is watching you. How strange: No one ever pays attention to you. You usually walk home, but maybe the bus would be safer today...\n")
+  choice1 = input("Enter A to take the bus or B to walk home: ").upper()
+  while choice1 != "A" and choice1 != "B":
+    print("\nThat is not a valid option.")
+    choice1 = input("\nEnter A to take the bus or B to walk home:  ").upper()
+  if choice1 == "A":
+    import bus
+  elif choice1 == "B":
+    import walk
+else:
+  print("You're really missing out...")
 
